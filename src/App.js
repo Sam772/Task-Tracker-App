cf72d6182a8aae748dd100083eb50ab87ef5dac7
Sam772 <78389553+Sam772@users.noreply.github.com>
2021-12-23 14:28:50 +0000

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react/cjs/react.production.min';
+
+function Body() {
+  return (
+    <p>
+      Starting text
+    </p>
+  );
+}
+
+class Header extends Component {
+  render() {
+    return(
+      <header className="App-Header">
+        <h1 className='App-Title'>Task Tracker App</h1>
+      </header>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Body/>
     </div>
   );
 }
